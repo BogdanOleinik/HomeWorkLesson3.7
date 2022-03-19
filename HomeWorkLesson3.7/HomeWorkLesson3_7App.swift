b99e8268d11e79d1b821485c98c2fdb976b666f4
Bogdan Oleinik <bogdan.swedbe@yandex.ru>
2022-03-19 12:42:45 +0300

    Initial Commit

---
+++ HomeWorkLesson3.7/HomeWorkLesson3_7App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWorkLesson3_7App.swift
+//  HomeWorkLesson3.7
+//
+//  Created by Олейник Богдан on 19.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct HomeWorkLesson3_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
